48a073439eaf75c213a49b78582a2ef3935d2acb
Ivan <ivanmatic023@gmail.com>
2023-06-02 11:49:23 +0200

    autoLogout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -18,6 +18,7 @@ export interface AuthResponseData {
 })
 export class AuthService {
   user$ = new BehaviorSubject<User>(null);
+  private tokenExpirationTimer: any;
 
   constructor(private http: HttpClient, private router: Router) {}
 
@@ -79,46 +80,56 @@ export class AuthService {
       _token: string;
       _tokenExpirationDate: string;
     } = JSON.parse(localStorage.getItem('userData'));
-  
+
     console.log(userData);
-  
+
     if (!userData) {
-      console.log("Nemaa");
+      console.log('Nemaa');
       return;
     }
-  
-    console.log("ima");
-  
+
+    console.log('ima');
+
     const loadedUser = new User(
       userData.email,
       userData.userId,
       userData._token,
       new Date(userData._tokenExpirationDate)
     );
-  
-    console.log(loadedUser);
-    console.log(userData._token);
-  
+
+
     if (userData._token) {
       this.user$.next(loadedUser);
-      console.log("test");
+      const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime()
+      this.autoLogout( expirationDuration);
     }
   }
 
   odjaviSe() {
     this.user$.next(null);
     this.router.navigate(['./autentifikacija']);
+    localStorage.removeItem('userData');
+    if(this.tokenExpirationTimer){
+      clearTimeout(this.tokenExpirationTimer)
+    }
+    this.tokenExpirationTimer= null
   }
 
+  autoLogout(expirationDuration: number) {
+  this.tokenExpirationTimer =  setTimeout(() => {
+      this.odjaviSe();
+    }, expirationDuration);
+  }
   private handleAuthentication(
     email: string,
     userId: string,
     token: string,
-    expresIn: number
+    expiresIn: number
   ) {
-    const expirationDate = new Date(new Date().getTime() + expresIn * 1000);
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
     this.user$.next(user);
+    this.autoLogout(expiresIn *1000)
     localStorage.setItem('userData', JSON.stringify(user));
   }
 
