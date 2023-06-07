a9f9779382ec6b2054bde1a5850c6372f74d1bb0
Ivan <ivanmatic023@gmail.com>
2023-06-07 10:43:31 +0200

    Shared Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatTableModule } from '@angular/material/table';
-import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigacijaComponent } from './navigacija/navigacija.component';
-
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatCardModule } from '@angular/material/card';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { ImenikComponent } from './imenik/imenik.component';
-import { AppRoutingModule } from './app-routing.module';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { FormsModule } from '@angular/forms';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { UnosKontaktaComponent } from './imenik/unos-kontakta/unos-kontakta.component';
-import { MatSortModule } from '@angular/material/sort';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { DeleteDialogComponent } from './imenik/delete-dialog/delete-dialog.component';
-import { MatRadioModule } from '@angular/material/radio';
 import { AuthComponent } from './auth/auth.component';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { ProfilKorisnikaComponent } from './profil-korisnika/profil-korisnika.component';
 import { SnackbarNotifyComponent } from './snackbar-notify/snackbar-notify.component';
 
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
+
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -49,32 +31,12 @@ import { SnackbarNotifyComponent } from './snackbar-notify/snackbar-notify.compo
     SnackbarNotifyComponent,
   ],
   imports: [
-    AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatCardModule,
-    MatExpansionModule,
-    MatFormFieldModule,
-    MatTableModule,
-    ReactiveFormsModule,
-    MatDialogModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    FormsModule,
     HttpClientModule,
-    MatSortModule,
-    MatPaginatorModule,
-    MatSnackBarModule,
-    MatRadioModule,
-    MatProgressSpinnerModule,
     ReactiveFormsModule,
+    SharedModule,
+    AppRoutingModule,
   ],
   providers: [
     {
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,72 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatDialogModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatSnackBarModule,
+    MatRadioModule,
+    MatProgressSpinnerModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    CommonModule,
+    MatTableModule,
+    MatDialogModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatSnackBarModule,
+    MatRadioModule,
+    MatProgressSpinnerModule,
+    ReactiveFormsModule,
+    FormsModule
+  ]
+})
+export class SharedModule { }
